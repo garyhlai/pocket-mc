a69258b157d95f0be806006e96e8d45878acc9e4
Gary Lai <garylai@MacBook-Air-de-Gary.local>
2019-08-14 16:49:27 -0500

    can call related_words from suggestor now

--- server/script/related_words.py
+++ server/script/related_words.py
@@ -1,27 +1,23 @@
-
-
 import gensim
 
 gmodel = gensim.models.KeyedVectors.load_word2vec_format(
     "./GoogleNews-vectors-negative300.bin", binary=True, limit=20000)
 
-target = "apple"
-# potential rhymes
-poten = ["royal", "awful", "oral", "anial"]
-# a dict of words and their similarity
-similarity = {}
 
-for i in poten:
-    try:
-        similarity[i] = gmodel.similarity(i, target)
-    # pass in case word not in the word2vec vocab
-    except KeyError:
-        pass
+def related(target, poten, n):
+    similarity = {}
 
-# only take the closest related rhymes
-n = 2
-result = sorted(similarity, key=similarity.get, reverse=True)
-# if result is very long, we only take the n top items
-if n < len(similarity):
-    result = sorted(similarity, key=similarity.get, reverse=True)[:n]
-print(result)
+    for i in poten:
+        try:
+            similarity[i] = gmodel.similarity(i, target)
+        # pass in case word not in the word2vec vocab
+        except KeyError:
+            pass
+
+    # only take the closest related rhymes
+    result = sorted(similarity, key=similarity.get, reverse=True)
+    # if result is very long, we only take the n top items
+    if n < len(similarity):
+        result = sorted(similarity, key=similarity.get, reverse=True)[:n]
+    # print(result)
+    return result
--- server/script/suggestor.py
+++ server/script/suggestor.py
@@ -1,9 +1,12 @@
 import sys
 from rhymes import Rhymes
 import pdb
-
+from related_words import related
 
 word = sys.argv[1]
+
 rhymes = Rhymes()
-output = ' '.join(rhymes.ends_with(rhymes.pronunciation_of(word), 2))
-print(output)
+output = [x.lower()
+          for x in rhymes.ends_with(rhymes.pronunciation_of(word), 2)]
+
+print(related(word, output, 30))
